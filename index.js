40b2b128c1ea95004b125bc52a2e468d438a3b4d
xvflame1-ui <xvflame1@gmail.com>
2026-02-05 00:36:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`🤖 Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.toLowerCase() === "hi bot") {
+    message.reply("Hey 👋 I’m alive!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
